8e2aa0ac65158ac34573a69aa9fe7f7a37d114a9
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-07 19:07:30 +0000

    Add files via upload

---
+++ lower_case.py
@@ -0,0 +1,9 @@
+fruit ="banana"
+if "n" in fruit :
+    print ("letter is been found")
+    greet = "HELLO ZINOU"
+    r=greet.lower()
+    print(r)
+    print("HELLO WORLD".lower())
+    
+    type(fruit)
